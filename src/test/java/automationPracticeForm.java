1fd7093186049c738616029331127805551621ec
Maria_Nour <marinour@mail.ru>
2023-02-19 18:20:48 +0300

    added test

---
+++ src/test/java/automationPracticeForm.java
@@ -0,0 +1,27 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class authomationPracticeForm {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+
+        $("#firstName").setValue("Maria");
+        $("#lastName").setValue("Nour");
+        $("#userEmail").setValue("MN@mail.ru");
+        $("#genterWrapper").$(byText("Female")).click();
+    }
+}

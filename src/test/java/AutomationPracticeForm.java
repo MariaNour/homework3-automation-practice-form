2157160b3a3c47882dade6c9c3c110d3c87f8f62
Maria_Nour <marinour@mail.ru>
2023-02-19 21:26:33 +0300

    changed public class AutomationPracticeForm

--- src/test/java/AutomationPracticeForm.java
+++ src/test/java/AutomationPracticeForm.java
@@ -1,16 +1,13 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byAttribute;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class automationPracticeForm {
+public class AutomationPracticeForm {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";

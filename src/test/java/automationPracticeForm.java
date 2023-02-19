b0663861219886e5bbaf44502fdb03cecb104659
Maria_Nour <marinour@mail.ru>
2023-02-19 20:19:19 +0300

    Homework 3 - done

--- src/test/java/automationPracticeForm.java
+++ src/test/java/automationPracticeForm.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byAttribute;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -22,5 +24,33 @@ public class automationPracticeForm {
         $("#firstName").setValue("Maria");
         $("#lastName").setValue("Nour");
         $("#userEmail").setValue("MN@mail.ru");
+        $("#gender-radio-2").sendKeys(" ");
+        $("#userNumber").setValue("9377777777");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-dropdown-container").$(byText("April")).click();
+        $(".react-datepicker__year-dropdown-container").$(byText("1994")).click();
+        $(".react-datepicker__month-container").$(byText("1")).click();
+        $("#subjectsContainer").click();
+        $("#subjectsInput").setValue("Arts").pressEnter();
+        $("#hobbies-checkbox-2").parent().$(byText("Reading")).click();
+        $("#uploadPicture").val("D:\\Alf.png");
+        $("#currentAddress").setValue("Lipchanskogo 5, apartment 22");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-body").shouldHave(text("Maria Nour"));
+        $(".modal-body").shouldHave(text("MN@mail.ru"));
+        $(".modal-body").shouldHave(text("Female"));
+        $(".modal-body").shouldHave(text("9377777777"));
+        $(".modal-body").shouldHave(text("01 April,1994"));
+        $(".modal-body").shouldHave(text("Arts"));
+        $(".modal-body").shouldHave(text("Reading"));
+        $(".modal-body").shouldHave(text("Alf.png"));
+        $(".modal-body").shouldHave(text("Lipchanskogo 5, apartment 22"));
+        $(".modal-body").shouldHave(text("NCR Delhi"));
+        $("#closeLargeModal").click();
        }
 }
